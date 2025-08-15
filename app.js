e1acd36df8e57edb0b03ca18dd7a5e1452eaa2b5
fareez555 <mazlaan134@gmail.com>
2025-08-15 08:03:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('FAARIZ BOT Alternative Server 🚀');
+app.get("/", (req, res) => {
+    res.send("FAARIZ BOT SERVER IS RUNNING 🚀");
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
